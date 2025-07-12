90d286a8be1f365885477f731be571b3e758a890
Johanna Haffner <johanna.haffner@bsse.ethz.ch>
2025-07-12 16:37:04 +0200

    update benchmarks to current state of the PR

--- benchmarks/conftest.py
+++ benchmarks/conftest.py
@@ -1,12 +1,14 @@
-import equinox.internal as eqxi
-import jax
-import pytest
+import os  # noqa: I001
+
+os.environ["EQX_ON_ERROR"] = "nan"  # Make sure this is set before importing equinox
+import equinox.internal as eqxi  # noqa: E402
+import jax  # noqa: E402
+import pytest  # noqa: E402
 
 
 jax.config.update("jax_enable_x64", True)
-#jax.config.update("jax_numpy_rank_promotion", "raise")
-jax.config.update("jax_numpy_dtype_promotion", "standard")
-# Remark Peter: limit JAX to single-thread with taskset -c 0 pytest --cutest
+jax.config.update("jax_numpy_rank_promotion", "raise")
+jax.config.update("jax_numpy_dtype_promotion", "strict")
 
 
 @pytest.fixture
@@ -16,9 +18,28 @@ def getkey():
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--cutest",
+        "--scipy",
         action="store_true",
-        dest="cutest",
+        dest="scipy",
         default=False,
-        help="Enable benchmark (CUTEST) tests.",
+        help="Enable comparison against scipy solvers on benchmark (CUTEST) tests.",
     )
+    parser.addoption(
+        "--max-dimension",
+        action="store",
+        type=int,
+        default=None,
+        help=(
+            "Maximum dimension for optimization variables. "
+            "Tests with higher dimensions will be skipped."
+        ),
+    )
+
+
+def pytest_configure(config):
+    global _max_dimension
+    _max_dimension = config.getoption("--max-dimension")
+
+
+def get_max_dimension():
+    return _max_dimension

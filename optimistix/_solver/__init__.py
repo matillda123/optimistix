967774b7314e03c8c4f7b68764d5d6072dcb5951
Till Stehling <tilljakobstehling@gmail.com>
2025-09-03 14:28:38 +0200

    Changed to correct file version

--- optimistix/_solver/__init__.py
+++ optimistix/_solver/__init__.py
@@ -6,17 +6,9 @@ from .best_so_far import (
     BestSoFarRootFinder as BestSoFarRootFinder,
 )
 from .bisection import Bisection as Bisection
-from .broyden import BadBroyden as BadBroyden, Broyden as Broyden
-from .brent import (
-    Brent as Brent,
-    GoldenSectionSearch as GoldenSectionSearch, 
-    Jarratt as Jarratt
-)
-from .brent_dekker import (
-    BrentDekker as BrentDekker,
-    InverseQuadraticInterpolation as InverseQuadraticInterpolation, 
-    Secant1D as Secant1D
-)
+from .brent import GoldenSectionSearch as GoldenSectionSearch, Jarratt as Jarratt, Brent as Brent
+from .brent_dekker import InverseQuadraticInterpolation as InverseQuadraticInterpolation, Secant1D as Secant1D, BrentDekker as BrentDekker
+from .broyden import Broyden as Broyden, BadBroyden as BadBroyden
 from .dogleg import Dogleg as Dogleg, DoglegDescent as DoglegDescent
 from .fixed_point import FixedPointIteration as FixedPointIteration
 from .gauss_newton import (
@@ -27,18 +19,9 @@ from .gauss_newton import (
 from .gradient_methods import (
     AbstractGradientDescent as AbstractGradientDescent,
     GradientDescent as GradientDescent,
-    ScaledGradientDescent as ScaledGradientDescent,
     SteepestDescent as SteepestDescent,
 )
-from .learning_rate import (
-    LearningRate as LearningRate,
-    pade_10 as pade_10,
-    pade_20 as pade_20,
-    pade_11 as pade_11,
-    pade_01 as pade_01,
-    pade_02 as pade_02,
-    ScaledLearningRate as ScaledLearningRate,
-)
+from .learning_rate import LearningRate as LearningRate
 from .levenberg_marquardt import (
     DampedNewtonDescent as DampedNewtonDescent,
     IndirectDampedNewtonDescent as IndirectDampedNewtonDescent,
